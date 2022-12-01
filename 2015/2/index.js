1e3409afe4d593384e12a1f49e87e99d592d3b28
Mindaugas <mindaugas.kurlys@gmail.com>
2022-12-02 00:55:16 +0200

    adventofcode 2015

--- 2015/2/index.js
+++ 2015/2/index.js
@@ -1,36 +1,30 @@
-const fs = require('fs')
+const fs = require("fs");
 
+const prog = (lines) => {
+  let area = 0;
+  let ribbon = 0;
+  lines.forEach((line) => {
+    const [height, width, length] = line.split("x");
+    const area1 = height * width;
+    const area2 = height * length;
+    const area3 = length * width;
+    const additional = Math.min(area1, area2, area3);
+    let [x, y] = [height, width, length].sort((a, b) => a - b).slice(0, 2);
+    const bond = height * width * length;
+    debugger;
+    ribbon += Number(x) + Number(x) + Number(y) + Number(y) + bond * 1;
+    area += 2 * (area1 + area2 + area3) + additional;
+    console.log({ height, width, length }, { x, y });
+  });
+  return [area, ribbon];
+};
 
-const part1 = (input) => {
-  let floors = 0
-  input.forEach(floor => floor === ('(') ? floors++ : floors--)
-  return floors
-}
-
-const part2 = (input) => {
-  let floors = 0;
-  let i = 0;
-  for (const floor of input){ 
-    floor === ('(') ? floors++ : floors--;
-    if (floors === -1) {
-      break;
-    };
-    i++;
-  }
-
-  return i +1;
-}
-
-fs.readFile('./input.txt', 'utf8' , (err, data) => {
+fs.readFile("./input.txt", "utf8", (err, data) => {
   if (err) {
-    console.error(err)
-    return
+    console.error(err);
+    return;
   }
-  // const lines = data.split('\n')
-  const input = [...data]
-
-  const answer1 = part1(input);
-  const answer2 = part2(input);
-
-  console.log({answer1, answer2})
-})
+  const lines = data.split("\n");
+  const [answer1, answer2] = prog(lines);
+  console.log({ answer1, answer2 });
+});
