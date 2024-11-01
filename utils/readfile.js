4eed680b6d3bae1450398b5f361001dcd59615a7
Mindaugas <mindaugas.kurlys@gmail.com>
2024-11-01 20:09:45 +0200

    initial

--- utils/readfile.js
+++ utils/readfile.js
@@ -1,14 +1,15 @@
-const fs = require('fs')
+const fs = require("fs");
 
 const readFile = async (file) => {
+  console.log("Reading from file");
   return new Promise((resolve, reject) => {
-    fs.readFile(file, 'utf8', function (err, data) {
+    fs.readFile(file, "utf8", function (err, data) {
       if (err) {
         reject(err);
       }
       resolve(data);
     });
   });
-}
+};
 
 module.exports = readFile;
