e8c7f575ac99b026ae0871f0635b1be8943fcbfd
mindaugaswashere <mindaugas.kurlys@gmail.com>
2024-11-01 22:04:54 +0200

    test commit

--- utils/readfile.js
+++ utils/readfile.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 
 const readFile = async (file) => {
   return new Promise((resolve, reject) => {
+    console.log("just a test");
     fs.readFile(file, "utf8", function (err, data) {
       if (err) {
         reject(err);
