0fd0b4b0c474ecfa10a900d1aa3aa0268e46bee6
Mindaugas <mindaugas.kurlys@gmail.com>
2024-11-01 20:18:38 +0200

    remove console logs

--- utils/readfile.js
+++ utils/readfile.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 
 const readFile = async (file) => {
-  console.log("Reading from file");
   return new Promise((resolve, reject) => {
     fs.readFile(file, "utf8", function (err, data) {
       if (err) {
