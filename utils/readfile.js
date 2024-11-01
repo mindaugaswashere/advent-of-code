376a4cb1d0b78fba6b0ad3cd6ff128012d423e5b
Mindaugas <mindaugas.kurlys@gmail.com>
2024-11-01 20:26:08 +0200

    nothing

--- utils/readfile.js
+++ utils/readfile.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 
 const readFile = async (file) => {
   return new Promise((resolve, reject) => {
-    console.log("");
     fs.readFile(file, "utf8", function (err, data) {
       if (err) {
         reject(err);
